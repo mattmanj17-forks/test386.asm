43ba7c0b3c4b167d7d76422b6e0ff4e85132bec7
superfury <superfury@live.nl>
2026-03-15 18:59:05 +0100

    - Implemented 32-bit protected mode interrupt stack tests using kernel mode only memory and pointers.

--- src/protected_rings_p.asm
+++ src/protected_rings_p.asm
@@ -54,6 +54,51 @@ switchToRing3:
 	push   dword edx                ; push return EIP
 	iretd
 
+;
+; Switches from Ring 0 to Ring 3, to return to ring 0 in flat mode later
+;
+; After calling this procedure consider all the registers and flags as trashed.
+; Also, the stack will be different, so saving the CPU state there will be pointless.
+;
+switchToRing3FLATkernel:
+	; In order to swich to user mode (ring 3) we need to execute an IRET with these
+	; values on the stack:
+	; - the instruction to continue execution at - the value of EIP.
+	; - the code segment selector to change to.
+	; - the value of the EFLAGS register to load.
+	; - the stack pointer to load.
+	; - the stack segment selector to change to.
+	; We also need:
+	; - a 32bit code descriptor in GDT with DPL 3
+	; - a 32bit data descriptor in GDT with DPL 3 (for the new stack)
+	; - to put the ring 0 stack in TSS.SS0 and TSS.ESP0
+	testCPL 0 ; we must be in ring 0
+	pop    edx ; read the return offset
+	mov    ax, ds
+	lds    ebx, [cs:ptrTSSprot]
+	; save ring 0 data segments, they'll be restored with switchToRing0
+	mov    [ebx+0x54], ax ; save DS
+	mov    ax, es
+	mov    [ebx+0x48], ax ; save ES
+	mov    ax, fs
+	mov    [ebx+0x58], ax ; save FS
+	mov    ax, gs
+	mov    [ebx+0x5C], ax ; save GS
+	; set ring 0 SS:ESP
+	mov    [ebx+4], esp
+	pushfd
+	or     dword [ebx+4], 0xE0010000      ; make sure we return to a proper ring0 stack
+	popfd
+	mov    eax, D_SEG_PROT32FLAT
+	mov    [ebx+8], eax
+	cli                             ; disable ints during switching
+	push   dword SU_SEG_PROT32|3    ; push user stack with RPL=3
+	push   dword ESP_R3_PROT        ; push user mode esp
+	pushfd                          ; push eflags
+	or     dword [ss:esp], 0x200    ; reenable interrupts in ring 3 (can't use privileged sti)
+	push   dword CU_SEG_PROT32|3    ; push user code segment with RPL=3
+	push   dword edx                ; push return EIP
+	iretd	
 
 ;
 ; Switches from Ring 0 to Ring 3 in V86 mode
--- src/test386.asm
+++ src/test386.asm
@@ -74,7 +74,8 @@
 ;  01000-01FFF page directory
 ;  02000-02FFF page table 0
 ;  03000-03FFF page table 1
-;  04000-04FFF TSS
+;  04000-04FFF page table 2 (kernel mode)
+;  05000-05FFF TSS
 ;  10000-1FFFF stack
 ;  20000-9FFFF tests
 ;
@@ -246,6 +247,7 @@ cpuTest:
 	jmp initGDT
 
 ESP_R0_PROT equ 0x0000FFFF
+ESP_R0_PROTFLAT equ 0xE001FFFF
 ESP_R3_PROT equ 0x00007FFF
 
 %include "protected_m.asm"
@@ -271,16 +273,18 @@ initGDT:
 	defGDTDesc NULL
 	defGDTDesc C_SEG_PROT16,  0x000f0000,0x0000ffff,ACC_TYPE_CODE_R|ACC_PRESENT
 	defGDTDesc C_SEG_PROT32,  0x000f0000,0x0000ffff,ACC_TYPE_CODE_R|ACC_PRESENT,EXT_32BIT
+	defGDTDesc C_SEG_PROT32FLAT,  0x00000000,0x000fffff,ACC_TYPE_CODE_R|ACC_PRESENT,EXT_32BIT|EXT_PAGE
 	defGDTDesc CU_SEG_PROT32, 0x000f0000,0x0000ffff,ACC_TYPE_CODE_R|ACC_PRESENT|ACC_DPL_3,EXT_32BIT
 	defGDTDesc CC_SEG_PROT32, 0x000f0000,0x0000ffff,ACC_TYPE_CODE_R|ACC_TYPE_CONFORMING|ACC_PRESENT|EXT_32BIT
-	defGDTDesc IDT_SEG_PROT,  0x00000400,0x0000012F,ACC_TYPE_DATA_W|ACC_PRESENT
+	defGDTDesc IDT_SEG_PROT,  0x00000400,0x00000137,ACC_TYPE_DATA_W|ACC_PRESENT
 	defGDTDesc IDTU_SEG_PROT, 0x00000400,0x0000012F,ACC_TYPE_DATA_W|ACC_PRESENT|ACC_DPL_3
 	defGDTDesc GDT_DSEG_PROT, 0x00000600,0x000002ff,ACC_TYPE_DATA_W|ACC_PRESENT
 	defGDTDesc GDTU_DSEG_PROT,0x00000600,0x000002ff,ACC_TYPE_DATA_W|ACC_PRESENT|ACC_DPL_3
 	defGDTDesc LDT_SEG_PROT,  0x00000900,0x000006ff,ACC_TYPE_LDT|ACC_PRESENT
 	defGDTDesc LDT_DSEG_PROT, 0x00000900,0x000006ff,ACC_TYPE_DATA_W|ACC_PRESENT
-	defGDTDesc PG_SEG_PROT,   0x00001000,0x00002fff,ACC_TYPE_DATA_W|ACC_PRESENT
+	defGDTDesc PG_SEG_PROT,   0x00001000,0x00003fff,ACC_TYPE_DATA_W|ACC_PRESENT
 	defGDTDesc S_SEG_PROT32,  0x00010000,0x0008ffff,ACC_TYPE_DATA_W|ACC_PRESENT,EXT_32BIT
+	defGDTDesc D_SEG_PROT32FLAT,  0x00000000,0x000fffff,ACC_TYPE_DATA_W|ACC_PRESENT,EXT_32BIT|EXT_PAGE
 	defGDTDesc SU_SEG_PROT32, 0x00010000,0x0008ffff,ACC_TYPE_DATA_W|ACC_PRESENT|ACC_DPL_3,EXT_32BIT
 	defGDTDesc TSS_PROT,      0x00004000,0x00000067,ACC_TYPE_TSS|ACC_PRESENT|ACC_DPL_3
 	defGDTDesc TSS_DSEG_PROT, 0x00004000,0x00000067,ACC_TYPE_DATA_W|ACC_PRESENT
@@ -314,14 +318,20 @@ ptrPT0prot: ; pointer to Page Table 0
 ptrPT1prot: ; pointer to Page Table 1
 	dd 0x2000
 	dw PG_SEG_PROT
+ptrPT2prot: ; pointer to Page Table 2 (upper half kernel)
+	dd 0x3000
+	dw PG_SEG_PROT
 ptrSSprot: ; pointer to the stack for pmode
 	dd ESP_R0_PROT
 	dw S_SEG_PROT32
+ptrSSprotFLAT: ; pointer to the stack for pmode
+	dd ESP_R0_PROTFLAT
+	dw D_SEG_PROT32FLAT
 ptrTSSprot: ; pointer to the task state segment
 	dd 0
 	dw TSS_DSEG_PROT
 addrProtIDT: ; address of pmode IDT to be used with lidt
-	dw 0x12F              ; 16-bit limit
+	dw 0x137              ; 16-bit limit
 	dd IDT_SEG_REAL << 4 ; 32-bit base address
 addrGDT: ; address of GDT to be used with lgdt
 	dw GDT_SEG_LIMIT
@@ -382,6 +392,13 @@ initIDT:
 	mov    dx,  ACC_DPL_3
 	inc    eax
 	call   initIntGateReal
+	; Interrupt 26h: non-conforming kernel interrupt, flat memory, callable from user mode
+	mov    esi, C_SEG_PROT32FLAT
+	mov    edi, kernelInterruptRestoreKernelStack
+	or     edi,0xE00F0000 ;Make sure that the offset is located on a valid 32-bit mapped address by mapping the high 16 bits.
+	mov    dx,  ACC_DPL_3
+	inc    eax
+	call   initIntGateReal
 
 	jmp initPaging
 
@@ -390,9 +407,10 @@ initPaging:
 ; pages:
 ;  00000-00FFF   1  1000h   4K IDTs, GDT and LDT
 ;  01000-01FFF   1  1000h   4K page directory
-;  02000-02FFF   1  1000h   4K page table 0
+;  02000-02FFF   1  1000h   4K page table 0 (user mode at 00000000 linear)
 ;  03000-03FFF   1  1000h   4K page table 1
-;  04000-04FFF   1  1000h   4K task switch segments
+;  04000-04FFF   1  1000h   4K page table 2 (kernel mode at E0000000 linear)
+;  05000-05FFF   1  1000h   4K task switch segments
 ;  05000-0FFFF  11  c000h  44K free
 ;  10000-1FFFF  16 10000h  64K stack
 ;  20000-9EFFF 127 7f000h 508K tests
@@ -401,6 +419,7 @@ initPaging:
 PAGE_DIR_ADDR equ 0x1000
 PAGE_TBL0_ADDR equ PAGE_DIR_ADDR+0x1000
 PAGE_TBL1_ADDR equ PAGE_DIR_ADDR+0x2000
+PAGE_TBL2_ADDR equ PAGE_DIR_ADDR+0x3000
 
 ;   Now we want to build a page directory and a page table. We need two pages of
 ;   4K-aligned physical memory.  We use a hard-coded address, segment 0x100,
@@ -421,6 +440,7 @@ PAGE_TBL1_ADDR equ PAGE_DIR_ADDR+0x2000
 	mov   ecx, 1024-1 ; ECX == number of (remaining) PDEs to write
 	xor   eax, eax    ; fill remaining PDEs with 0
 	rep   stosd
+
 ;
 ;   Build a page table at EDI with 256 (out of 1024) valid PTEs, mapping the first 1MB
 ;   as linear == physical.
@@ -434,6 +454,28 @@ PAGE_TBL1_ADDR equ PAGE_DIR_ADDR+0x2000
 	mov   ecx, 1024-256 ; ECX == number of (remaining) PTEs to write
 	xor   eax, eax
 	rep   stosd
+
+;
+;   Build a page table at EDI with 256 (out of 1024) valid PTEs, mapping the kernel 1MB
+;   as high linear == physical low, using kernel mode pages.
+;
+	mov   eax, PTE_PRESENT | PTE_WRITE
+	mov   ecx, 256 ; ECX == number of PTEs to write
+	mov   edi, 0x3000 ;Kernel mode PDT base address
+.initPT2:
+	stosd
+	add   eax, 0x1000
+	loop  .initPT2
+	mov   ecx, 1024-256 ; ECX == number of (remaining) PTEs to write
+	xor   eax, eax
+	rep   stosd
+
+;
+; Map the kernel mode 1MB memory at E0000000 using the created PT2 map within the page directory.
+;
+	mov   eax, PAGE_TBL2_ADDR | PTE_PRESENT | PTE_WRITE
+	mov   [es:0xE00], eax
+
 
 switchToProtMode:
 	cli ; make sure interrupts are off now, since we've not initialized the IDT yet
@@ -468,6 +510,7 @@ initLDT:
 	lldt ax
 	mov  ax, TSS_PROT
 	ltr  ax
+	loadProtModeStack ;Make sure that the stack is valid for our tests.
 	jmp  protTests
 
 %include "tss_p.asm"
@@ -717,6 +760,16 @@ kernelModeOnlyInterruptReturn:
 	mov  eax, esp  ; Save the stack pointer for us to check, as the interrupt doesn't have a comparison.
 	int  0x24
 kernelOnlyConformingInterruptReturn:
+
+	; Test user to kernel stack switch using different address spaces
+	; Interrupt from user mode to kernel mode (flat address space)
+	call  switchToRing3FLATkernel ;Switch to ring 3 with flat kernel stack prepared
+	int   0x26
+kernelModeInterruptKernelStackReturn:
+	push kernelModeInterruptKernelStackReturnPoint
+	call  switchToRing0
+	kernelModeInterruptKernelStackReturnPoint:
+	; Back in normal 32-bit protected mode with 16-bit segments again
 
 	;
 	; Validate Virtual-8086 mode.
